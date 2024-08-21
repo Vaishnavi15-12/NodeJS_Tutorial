fd3c7d0985c85b44c52bbd57927b9771b5047be2
Vaishu <vaishnavisatrasala1997@gmail.com>
2024-08-21 10:45:52 -0400

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//Modules
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavour');
+
+require('./7-mind-grenede');
+
+console.log(names);
+
+sayHi('Baby');
+sayHi(names.vaish);
+sayHi(names.vinay);
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'Super SECRET';
+
+//share
+const vaish = 'vaish';
+const vinay = 'vinay';
+
+module.exports = {vaish,vinay}
+
